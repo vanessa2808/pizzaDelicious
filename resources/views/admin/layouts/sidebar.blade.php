863be790db5e5dce25ae28e6f72996edc30954eb
VanessaNguyen <yenrion9941@gmail.com>
2020-03-29 14:53:00 +0700

    [create][]create front end admin

---
+++ resources/views/admin/layouts/sidebar.blade.php
@@ -0,0 +1,241 @@
+
+<div class="left-sidebar-pro">
+    <nav id="sidebar">
+        <div class="sidebar-header">
+            <a href="#"><img src="admin/img/message/1.jpg" alt="" />
+            </a>
+            <h3>Andrar Son</h3>
+            <p>Developer</p>
+            <strong>AP+</strong>
+        </div>
+        <div class="left-custom-menu-adp-wrap">
+            <ul class="nav navbar-nav left-sidebar-menu-pro">
+                <li class="nav-item">
+                    <a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-home"></i> <span class="mini-dn">Header</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown animated flipInX">
+                        <a href="#" class="dropdown-item">Add_header</a>
+                        <a href="#" class="dropdown-item">List_header</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-envelope"></i> <span class="mini-dn">Activities</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown animated flipInX">
+                        <a href="#" class="dropdown-item">Add_activities</a>
+                        <a href="#" class="dropdown-item">List_activity</a>
+                        <a href="#" class="dropdown-item">List_image</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-flask"></i> <span class="mini-dn">address</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown animated flipInX">
+                        <a href="#" class="dropdown-item">Add address</a>
+                        <a href="#" class="dropdown-item">List address</a>
+
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-pie-chart"></i> <span class="mini-dn">Food</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown animated flipInX">
+                        <a href="#" class="dropdown-item">Add food</a>
+                        <a href="#" class="dropdown-item">List food</a>
+
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-bar-chart-o"></i> <span class="mini-dn">Chef</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown chart-left-menu-std animated flipInX">
+                        <a href="#" class="dropdown-item">Add chef</a>
+                        <a href="#" class="dropdown-item">List chef</a>
+
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-table"></i> <span class="mini-dn">Menu</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown animated flipInX">
+                        <a href="#" class="dropdown-item">Add menu</a>
+                        <a href="#" class="dropdown-item">List menu</a>
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-edit"></i> <span class="mini-dn">Menu pricing</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown form-left-menu-std animated flipInX">
+                        <a href="#" class="dropdown-item">Add menuPricing</a>
+                        <a href="#" class="dropdown-item">List menuPricing</a>
+
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-desktop"></i> <span class="mini-dn">Function services</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown apps-left-menu-std animated flipInX">
+                        <a href="#" class="dropdown-item">Add function services</a>
+                        <a href="#" class="dropdown-item">List function services</a>
+                        <a href="#" class="dropdown-item">List number of function services</a>
+
+
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-files-o"></i> <span class="mini-dn">Blog</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown pages-left-menu-std animated flipInX">
+                        <a href="#" class="dropdown-item">Add blog</a>
+                        <a href="#" class="dropdown-item">List BLog</a>
+
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-edit"></i> <span class="mini-dn">Contact Information</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown form-left-menu-std animated flipInX">
+                        <a href="#" class="dropdown-item">Add contact</a>
+                        <a href="#" class="dropdown-item">List contact</a>
+
+                    </div>
+                </li>
+                <li class="nav-item"><a href="#" data-toggle="dropdown" role="button" aria-expanded="false" class="nav-link dropdown-toggle"><i class="fa big-icon fa-files-o"></i> <span class="mini-dn">Users</span> <span class="indicator-right-menu mini-dn"><i class="fa indicator-mn fa-angle-left"></i></span></a>
+                    <div role="menu" class="dropdown-menu left-menu-dropdown pages-left-menu-std animated flipInX">
+                        <a href="#" class="dropdown-item">Add users</a>
+                        <a href="#" class="dropdown-item">List users</a>
+
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</div>
+<div class="mobile-menu-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-md-12 col-sm-12 col-xs-12">
+                <div class="mobile-menu">
+                    <nav id="dropdown">
+                        <ul class="mobile-menu-nav">
+                            <li><a data-toggle="collapse" data-target="#Charts" href="#">Home <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul class="collapse dropdown-header-top">
+                                    <li><a href="dashboard.html">Dashboard v.1</a>
+                                    </li>
+                                    <li><a href="dashboard-2.html">Dashboard v.2</a>
+                                    </li>
+                                    <li><a href="analytics.html">Analytics</a>
+                                    </li>
+                                    <li><a href="widgets.html">Widgets</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#demo" href="#">Mailbox <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="demo" class="collapse dropdown-header-top">
+                                    <li><a href="inbox.html">Inbox</a>
+                                    </li>
+                                    <li><a href="view-mail.html">View Mail</a>
+                                    </li>
+                                    <li><a href="compose-mail.html">Compose Mail</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#others" href="#">Miscellaneous <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="others" class="collapse dropdown-header-top">
+                                    <li><a href="profile.html">Profile</a>
+                                    </li>
+                                    <li><a href="contact-client.html">Contact Client</a>
+                                    </li>
+                                    <li><a href="contact-client-v.1.html">Contact Client v.1</a>
+                                    </li>
+                                    <li><a href="project-list.html">Project List</a>
+                                    </li>
+                                    <li><a href="project-details.html">Project Details</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#Miscellaneousmob" href="#">Interface <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="Miscellaneousmob" class="collapse dropdown-header-top">
+                                    <li><a href="google-map.html">Google Map</a>
+                                    </li>
+                                    <li><a href="data-maps.html">Data Maps</a>
+                                    </li>
+                                    <li><a href="pdf-viewer.html">Pdf Viewer</a>
+                                    </li>
+                                    <li><a href="x-editable.html">X-Editable</a>
+                                    </li>
+                                    <li><a href="code-editor.html">Code Editor</a>
+                                    </li>
+                                    <li><a href="tree-view.html">Tree View</a>
+                                    </li>
+                                    <li><a href="preloader.html">Preloader</a>
+                                    </li>
+                                    <li><a href="images-cropper.html">Images Cropper</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#Chartsmob" href="#">Charts <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="Chartsmob" class="collapse dropdown-header-top">
+                                    <li><a href="bar-charts.html">Bar Charts</a>
+                                    </li>
+                                    <li><a href="line-charts.html">Line Charts</a>
+                                    </li>
+                                    <li><a href="area-charts.html">Area Charts</a>
+                                    </li>
+                                    <li><a href="rounded-chart.html">Rounded Charts</a>
+                                    </li>
+                                    <li><a href="c3.html">C3 Charts</a>
+                                    </li>
+                                    <li><a href="sparkline.html">Sparkline Charts</a>
+                                    </li>
+                                    <li><a href="peity.html">Peity Charts</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#Tablesmob" href="#">Tables <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="Tablesmob" class="collapse dropdown-header-top">
+                                    <li><a href="static-table.html">Static Table</a>
+                                    </li>
+                                    <li><a href="data-table.html">Data Table</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#formsmob" href="#">Forms <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="formsmob" class="collapse dropdown-header-top">
+                                    <li><a href="basic-form-element.html">Basic Form Elements</a>
+                                    </li>
+                                    <li><a href="advance-form-element.html">Advanced Form Elements</a>
+                                    </li>
+                                    <li><a href="password-meter.html">Password Meter</a>
+                                    </li>
+                                    <li><a href="multi-upload.html">Multi Upload</a>
+                                    </li>
+                                    <li><a href="tinymc.html">Text Editor</a>
+                                    </li>
+                                    <li><a href="dual-list-box.html">Dual List Box</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#Appviewsmob" href="#">App views <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="Appviewsmob" class="collapse dropdown-header-top">
+                                    <li><a href="basic-form-element.html">Basic Form Elements</a>
+                                    </li>
+                                    <li><a href="advance-form-element.html">Advanced Form Elements</a>
+                                    </li>
+                                    <li><a href="password-meter.html">Password Meter</a>
+                                    </li>
+                                    <li><a href="multi-upload.html">Multi Upload</a>
+                                    </li>
+                                    <li><a href="tinymc.html">Text Editor</a>
+                                    </li>
+                                    <li><a href="dual-list-box.html">Dual List Box</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li><a data-toggle="collapse" data-target="#Pagemob" href="#">Pages <span class="admin-project-icon adminpro-icon adminpro-down-arrow"></span></a>
+                                <ul id="Pagemob" class="collapse dropdown-header-top">
+                                    <li><a href="login.html">Login</a>
+                                    </li>
+                                    <li><a href="register.html">Register</a>
+                                    </li>
+                                    <li><a href="captcha.html">Captcha</a>
+                                    </li>
+                                    <li><a href="checkout.html">Checkout</a>
+                                    </li>
+                                    <li><a href="contact.html">Contacts</a>
+                                    </li>
+                                    <li><a href="review.html">Review</a>
+                                    </li>
+                                    <li><a href="order.html">Order</a>
+                                    </li>
+                                    <li><a href="comment.html">Comment</a>
+                                    </li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
--- resources/views/admin/users/add_users.blade.php
+++ resources/views/admin/users/add_users.blade.php
@@ -2,7 +2,107 @@
     @extends('admin.layouts.master')
 @section('title', 'admin')
 
+<div class="wrapper-pro">
 
+<div class="content-inner-all">
+    <div class="breadcome-area mg-b-30 small-dn">
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-lg-1 col-md-6 col-sm-6 col-xs-12">
+                    <button type="button" id="sidebarCollapse" class="btn bar-button-pro header-drl-controller-btn btn-info navbar-btn">
+                        <i class="fa fa-bars"></i>
+                    </button>
+                    <div class="admin-logo logo-wrap-pro">
+                        <a href="#"><img src="admin/img/logo/log.png" alt="" />
+                        </a>
+                    </div>
+                </div>
+                <div class="col-lg-12">
+                    <div class="breadcome-list map-mg-t-40-gl shadow-reset">
+                        <div class="row">
+                            <div class="col-lg-6">
+                                <div class="breadcome-heading">
+                                    <form role="search" class="">
+                                        <input type="text" placeholder="Search..." class="form-control">
+                                        <a href=""><i class="fa fa-search"></i></a>
+                                    </form>
+                                </div>
+                            </div>
+                            <div class="col-lg-6">
+                                <ul class="breadcome-menu">
+                                    <li><a href="#">Home</a> <span class="bread-slash">/</span>
+                                    </li>
+                                    <li><span class="bread-blod">Accordion</span>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    <div class="container-fluid">
+
+        <div class="row">
+            <div class="col-lg-12">
+                <div class="sparkline16-list shadow-reset mg-t-30">
+                    <div class="sparkline16-hd">
+                        <div class="main-sparkline16-hd">
+                            <h1>Data picker</h1>
+                            <div class="sparkline16-outline-icon">
+                                <span class="sparkline16-collapse-link"><i class="fa fa-chevron-up"></i></span>
+                                <span><i class="fa fa-wrench"></i></span>
+                                <span class="sparkline16-collapse-close"><i class="fa fa-times"></i></span>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="sparkline16-graph">
+                        <div class="date-picker-inner">
+                            <div class="form-group data-custon-pick" id="data_1">
+                                <label>Simple data input format</label>
+                                <div class="input-group date">
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    <input type="text" class="form-control" value="10/04/2018">
+                                </div>
+                            </div>
+                            <div class="form-group data-custon-pick" id="data_2">
+                                <label>One Year view</label>
+                                <div class="input-group date">
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    <input type="text" class="form-control" value="08/09/2018">
+                                </div>
+                            </div>
+                            <div class="form-group data-custon-pick" id="data_3">
+                                <label>Decade view</label>
+                                <div class="input-group date">
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    <input type="text" class="form-control" value="10/11/2013">
+                                </div>
+                            </div>
+                            <div class="form-group data-custon-pick" id="data_4">
+                                <label>Month select</label>
+                                <div class="input-group date">
+                                    <span class="input-group-addon"><i class="fa fa-calendar"></i></span>
+                                    <input type="text" class="form-control" value="07/01/2018">
+                                </div>
+                            </div>
+                            <div class="form-group data-custon-pick data-custom-mg" id="data_5">
+                                <label>Range select</label>
+                                <div class="input-daterange input-group" id="datepicker">
+                                    <input type="text" class="form-control" name="start" value="05/14/2018" />
+                                    <span class="input-group-addon">to</span>
+                                    <input type="text" class="form-control" name="end" value="05/22/2018" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+</div>
+
+</div>
+</div>
 
 @endsection
 
