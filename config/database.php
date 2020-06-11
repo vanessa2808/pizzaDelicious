92f9c5cec44513952207c7746aac505ff1f0b6f8
VanessaNguyen <yenrion9941@gmail.com>
2020-06-11 19:00:49 +0700

    deploy1

--- config/database.php
+++ config/database.php
@@ -3,7 +3,7 @@
 use Illuminate\Support\Str;
 
 return [
-
+    $DATABASE_URL=parse_url('postgres://mwouvvysbvlesr:38490b1a0d4b26f2a4f52e91185f35bb0751f18ce3f50234d91401b6956a1de4@ec2-34-198-243-120.compute-1.amazonaws.com:5432/dajrk5eeb3ppi7'),
     /*
     |--------------------------------------------------------------------------
     | Default Database Connection Name
@@ -15,7 +15,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'pgsql'),
 
     /*
     |--------------------------------------------------------------------------
@@ -66,11 +66,11 @@ return [
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOST', '127.0.0.1'),
-            'port' => env('DB_PORT', '5432'),
-            'database' => env('DB_DATABASE', 'forge'),
-            'username' => env('DB_USERNAME', 'forge'),
-            'password' => env('DB_PASSWORD', ''),
+            'host' => $DATABASE_URL["host"],
+            'port' => $DATABASE_URL["port"],
+            'database' => ltrim($DATABASE_URL["path"],"/"),
+            'username' => $DATABASE_URL["user"],
+            'password' => $DATABASE_URL["password"],
             'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
