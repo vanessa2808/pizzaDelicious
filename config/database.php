3fe5815f8f5de494c2f06ad071029157b7dbafe6
VanessaNguyen <yenrion9941@gmail.com>
2020-06-11 20:05:22 +0700

    update

--- config/database.php
+++ config/database.php
@@ -71,7 +71,7 @@ return [
             'database' => ltrim($DATABASE_URL["path"],"/"),
             'username' => $DATABASE_URL["user"],
             'password' => $DATABASE_URL["password"],
-            'charset' => 'utf8mb4',
+            'charset' => 'utf8',
             'prefix' => '',
             'prefix_indexes' => true,
             'schema' => 'public',
