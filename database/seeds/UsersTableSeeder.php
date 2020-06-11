b9cb9fda7d7f59c87b3c3aac88abc21b82124478
VanessaNguyen <yenrion9941@gmail.com>
2020-06-11 21:25:27 +0700

    update

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -14,6 +14,7 @@ class UsersTableSeeder extends Seeder
     public function run()
     {
         DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+        
         DB::table('users')->truncate();
         DB::table('users')->insert([
             [
