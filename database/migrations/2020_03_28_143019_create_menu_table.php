e62b890bd23effb946d9e2b1ba65d4e9137edbb7
VanessaNguyen <yenrion9941@gmail.com>
2020-06-11 21:58:13 +0700

    Add config files.

--- database/migrations/2020_03_28_143019_create_menu_table.php
+++ database/migrations/2020_03_28_143019_create_menu_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class endsCreateMenuTable extends Migration
+class CreateMenuTable extends Migration
 {
     /**
      * Run the migrations.
