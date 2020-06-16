efa792337db8566fd164349e621fd84f890db995
VanessaNguyen <yenrion9941@gmail.com>
2020-06-16 15:15:52 +0700

    update comment

--- resources/views/page/layouts/header.blade.php
+++ resources/views/page/layouts/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark ftco_navbar bg-dark ftco-navbar-light" id="ftco-navbar">
     <div class="container">
-        <a class="navbar-brand" href="/"><span class="flaticon-pizza-1 mr-1"></span>Pizza<br><small>Delicous</small></a>
+        <a class="navbar-brand" href="/"><span class="flaticon-pizza-1 mr-1"></span>Golden<br><small>restaurant</small></a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#ftco-nav" aria-controls="ftco-nav" aria-expanded="false" aria-label="Toggle navigation">
             <span class="oi oi-menu"></span> Menu
         </button>
